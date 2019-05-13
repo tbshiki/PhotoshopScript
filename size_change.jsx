23806ee067f4bab1b0b9d4234750a9287dc77a82
つぶ <40558067+tbshiki@users.noreply.github.com>
2019-05-13 15:13:09 +0900

    バグ修正
    高さ1200px以上、高さ1600px以下が適応外になっていたので修正

--- size_change.jsx
+++ size_change.jsx
@@ -1,4 +1,4 @@
-//ピクセル指定のおまじない
+﻿//ピクセル指定のおまじない
 var saveRulerUnits=preferences.rulerUnits;
 preferences.rulerUnits=Units.PIXELS;
 
@@ -15,16 +15,16 @@ var ratio=w/h; //縦横比を取得
 //新規サイズ
 var newW=0; //新規幅初期化
 var newH=0; //新規高さ初期化
- 
+
 if(w>widthLim || h>heightLim){
-    if(w>h){//幅の方が大きい
-        newW=widthLim;
-        newH=widthLim/ratio;
-    }else{//高さの方が大きい
+    if(h > 1200){//高さが1200を超えている
         newW=heightLim*ratio;
         newH=heightLim;
+    }else{//幅が1600を超えている
+        newW=widthLim;
+        newH=widthLim/ratio;
     }
     //サイズ変更
     baseFile.resizeImage(newW,newH,baseFile.resolution,ResampleMethod.BICUBIC);
 }
-preferences.rulerUnits=saveRulerUnits; //単位を元に戻す
+preferences.rulerUnits=saveRulerUnits; //ピクセル指定を元に戻す
